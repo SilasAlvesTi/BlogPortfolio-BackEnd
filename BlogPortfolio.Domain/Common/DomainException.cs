1ec91ff6023b350dcdbb496038744a1abd4be01b
SilasAlvesTi <silas.alves.sa04@gmail.com>
2026-01-19 19:55:27 -0300

    feat: Adicionando domínio de usuário

---
+++ BlogPortfolio.Domain/Common/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogPortfolio.Domain.Common
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ BlogPortfolio.Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using BlogPortfolio.Domain.Common;
+using BlogPortfolio.Domain.Enums;
+
+namespace BlogPortfolio.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public UserRole Role { get; private set; }
+
+        public User(string name, string email, string password, UserRole role = UserRole.User)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new DomainException("Name is required");
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new DomainException("Email is required");
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new DomainException("Password is required");
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+
+        public bool IsAdmin()
+            => Role == UserRole.Admin;
+    }
+}
