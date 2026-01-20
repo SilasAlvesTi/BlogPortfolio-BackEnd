1196627f0d77fe2af48a48ebc2c0d9011f5b7458
SilasAlvesTi <silas.alves.sa04@gmail.com>
2026-01-20 16:48:28 -0300

    feat: iniciando application com command para criar rascunho do post

---
+++ src/BlogPortfolio.Application/Common/Interfaces/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogPortfolio.Application.Common.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<Post?> GetByIdAsync(Guid id);
+        Task UpdateAsync(Post post);
+        Task AddAsync(Post post);
+    }
+}
---
+++ src/BlogPortfolio.Application/Posts/Commands/CreateDraftPost/CreateDraftPostCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using BlogPortfolio.Application.Common.Interfaces;
+using BlogPortfolio.Application.Common.Results;
+using FluentValidation;
+
+namespace BlogPortfolio.Application.Posts.Commands.CreateDraftPost
+{
+    public class CreateDraftPostCommandHandler
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IValidator<CreateDraftPostCommand> _validator;
+
+        public CreateDraftPostCommandHandler(
+            IPostRepository postRepository,
+            IUserRepository userRepository,
+            IValidator<CreateDraftPostCommand> validator)
+        {
+            _postRepository = postRepository;
+            _userRepository = userRepository;
+            _validator = validator;
+        }
+
+        public async Task<Result<Guid>> Handle(CreateDraftPostCommand command)
+        {
+            var validationResult = await _validator.ValidateAsync(command);
+
+            if (!validationResult.IsValid)
+            {
+                return Result<Guid>.Failure(validationResult.Errors[0].ErrorMessage);
+            }
+
+            var owner = await _userRepository.GetByIdAsync(command.OwnerId);
+            
+            if (owner is null)
+                return Result<Guid>.Failure("User not found");
+
+            var post = Post.CreateDraft(owner.Id);
+
+            await _postRepository.AddAsync(post);
+
+            return Result<Guid>.Success(post.Id);
+        }
+    }
+}
